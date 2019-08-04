264bb1bb428b80d08176dca6ffa340a41f1446da
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2019-08-04 11:36:45 +0300

    Use Strings instead of Text

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,12 @@
 {-# LANGUAGE BlockArguments      #-}
-{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns        #-}
 module Main where
 
-import           Control.Monad        (when)
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.UTF8 as B
 import           Data.Char            (toLower)
 import           Data.Foldable        (for_)
-import           Data.List            (filter, isInfixOf)
-import           Data.Maybe
-import qualified Data.Text            as T
-import           Data.Text.Encoding   (decodeUtf8)
+import           Data.List            (filter, intercalate, isInfixOf)
 import qualified Network.HTTP.Simple  as HTTP
 import           Safe                 (initMay, lastMay, readMay)
 import           System.Environment
@@ -39,27 +34,27 @@ printTopics pageNum keywords pageLimit
           url n = "https://www.digicamera.net/keskus/viewforum.php?f=10&start=" <> (show $ n * 25)
       initReq <- HTTP.parseRequest $ url pageNum
       r <- HTTP.httpBS initReq
-      let res = decodeUtf8 $ HTTP.getResponseBody r
+      let res = B.toString $ HTTP.getResponseBody r
       for_ keywords $ \keyword ->
-        putStrLn $ T.unpack $ T.intercalate "\n"
+        putStrLn $ intercalate "\n"
         $ highlightMatches keyword
         $ filterByKeyword keyword
         $ findTopics [] $ TS.parseTags res
       printTopics (pageNum + 1) keywords pageLimit
 
-findTopics :: [T.Text] -> [TS.Tag T.Text] -> [T.Text]
+findTopics :: [String] -> [TS.Tag String] -> [String]
 findTopics topics [] = topics
-findTopics topics (TS.TagOpen ("a" :: T.Text) [("href", _url), ("class", "topictitle")] : (TS.TagText topic) : rest) =
+findTopics topics (TS.TagOpen "a" [("href", _url), ("class", "topictitle")] : (TS.TagText topic) : rest) =
   findTopics (topics <> [topic]) rest
 findTopics topics (_:rest) = findTopics topics rest
 
-filterByKeyword :: String -> [T.Text] -> [T.Text]
+filterByKeyword :: String -> [String] -> [String]
 filterByKeyword (map toLower -> keyword) topics =
-  filter (isInfixOf keyword . T.unpack . T.toLower) topics
+  filter (isInfixOf keyword . map toLower) topics
 
-highlightMatches :: String -> [T.Text] -> [T.Text]
+highlightMatches :: String -> [String] -> [String]
 highlightMatches keyword matches =
- map (T.pack . highlightMatchingWord keyword "" . T.unpack) matches
+ map (highlightMatchingWord keyword "") matches
 
 highlightMatchingWord :: String -> String -> String -> String
 highlightMatchingWord _ final "" = final
